3f60f96026758285b8044f2254d27528d06ebe7b
wkratos22 <michaelwachnicki2001@gmail.com>
2022-04-04 15:55:11 +0200

    todolist non completo

---
+++ main.js
@@ -0,0 +1,16 @@
+var app = new Vue(
+    {
+        el: `#root`,
+        data: {
+            todos: [
+                {name: `testo della todo 1`, done: true, },
+                {name: `testo della todo 2`, done: false, },
+                {name: `testo della todo 3`, done: false, },
+                {name: `testo della todo 3`, done: true, }
+            ],
+        },
+        methods: {
+           
+        }    
+    }   
+);
